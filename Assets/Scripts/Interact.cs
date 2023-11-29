99ac8c952f594b78c4b5d3f8d2fe0f35fec597e4
Camila Gonella <camilamgonella@gmail.com>
2023-11-29 04:04:21 -0300

    Lever funcionando

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interact : MonoBehaviour, IInteractable
+public class Interact : MonoBehaviour, IInteractable
 {
-    InventoryItemData _item; // El objeto Inventory ItemData que deseas asignar
+    [SerializeField] GameObject _lever;
+    [SerializeField] Animator _leverAnim;
+    [SerializeField] GameObject _wallPrefab;
 
-    public virtual void AddToInventory()
+    private void Update()
     {
-        InventorySystem.current.Add(_item);
-        Debug.Log("Se agregp");
-        Destroy(gameObject);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OpenDor();
+        }
     }
+
+    public void OpenDor()
+    {
+        _leverAnim.SetBool("Open", true);
+        _wallPrefab.GetComponent<Animator>().SetBool("Open", true);
+
+        //StartCoroutine(DestroyWall());
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("abrete sesamo");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OpenDor();
+        }
+    }
+
+    //private IEnumerator DestroyWall()
+    //{
+    //    yield return new WaitForSeconds(3f);
+    //    Destroy(_wallPrefab);
+    //}
 }
