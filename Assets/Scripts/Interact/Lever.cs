8b86fb1a09c06907706aa22950d52063cb8c6eda
Camila Gonella <camilamgonella@gmail.com>
2023-12-01 18:27:11 -0300

    Sonido palanca y pared

--- Assets/Scripts/Interact/Lever.cs
+++ Assets/Scripts/Interact/Lever.cs
@@ -6,10 +6,15 @@ public class Lever : Interact
     [SerializeField] GameObject _lever;
     [SerializeField] Animator _leverAnim;
     [SerializeField] GameObject _wallPrefab;
+    [SerializeField] AudioSource _audioLever;
+    [SerializeField] AudioSource _audioWall;
 
     new private void Start()
     {
         base.Start();
+
+
+        _audioWall = _wallPrefab.GetComponent<AudioSource>();
     }
 
     protected override void InteractionAction()
@@ -19,17 +24,17 @@ public class Lever : Interact
 
     private void OpenDoor()
     {
-       
         float distance = Vector3.Distance(_player.transform.position, transform.position);
 
         if (distance < 2.0f && Input.GetKey(KeyCode.E))
         {
             _leverAnim.SetBool("Open", true);
             _wallPrefab.GetComponent<Animator>().SetBool("Open", true);
+            _audioLever.Play();
+            _audioWall.Play();
 
             StartCoroutine(DestroyWall());
-        }
-        
+        }  
     }
 
     private IEnumerator DestroyWall()
