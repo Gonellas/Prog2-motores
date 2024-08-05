eeb5055c9e1032cb98742cbbbf6775291509e329
Camila Gonella <camilamgonella@gmail.com>
2024-08-05 20:56:05 -0300

    Victory Scene

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public SceneManagerController sceneManagerController; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (sceneManagerController != null)
+            {
+                sceneManagerController.ChangeScene("Victory");
+            }
+        }
+    }
+}
